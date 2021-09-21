565fc37298b579bd0d762bd2b9d7238d8b727971
Javier Canizalez <javier-canizalez@outlook.com>
2021-09-21 10:14:50 -0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "google" {
+  project     = local.project
+}
+
+variable "name" {
+  description = "Name of the instance"
+  type        = string
+}
+
+resource "google_compute_instance" "default" {
+  name         = var.name
+  machine_type = "e2-small"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+}
+
+
+output "id" {
+   value = google_compute_instance.default.id
+}
